55008830f81cb17490e4d70af044c5d6ef70069a
Chiichen <chiichen@qq.com>
2024-07-08 19:56:58 +0800

    refactor!: rename `driver` to `recognizer`

--- cice-core/src/executor/task.rs
+++ cice-core/src/executor/task.rs
@@ -1,12 +1,12 @@
 use alloc::boxed::Box;
 
-use crate::{controller::Controller, driver::Driver};
+use crate::{controller::Controller, recognizer::Recognizer};
 
 use super::state::State;
 
 pub struct Task {
     pub(crate) from: State,
     pub(crate) to: State,
-    driver: Box<dyn Driver>,
+    recognizer: Box<dyn Recognizer>,
     controller: Box<dyn Controller>,
 }
