b33f2a765b9301b8cdd3a657f994b0dc760ecc39
Chiichen <chiichen@qq.com>
2025-01-25 17:38:37 +0800

    chore: fix nix settings for darwin

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     devenv.url = "github:cachix/devenv";
     fenix.url = "github:nix-community/fenix";
     fenix.inputs = { nixpkgs.follows = "nixpkgs"; };
-
+    utils.url = "github:numtide/flake-utils";
   };
 
   nixConfig = {
@@ -12,16 +12,13 @@
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, devenv, ... } @ inputs:
-    let
-      system = "x86_64-linux";
-
-      pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { self, nixpkgs, devenv, utils,  ... } @ inputs:
+     utils.lib.eachDefaultSystem (system:
+    let pkgs = nixpkgs.legacyPackages."${system}";
     in
     {
       packages.${system}.devenv-up = self.devShells.${system}.default.config.procfileScript;
-
-      devShells.${system}.default = devenv.lib.mkShell {
+      devShells.default = devenv.lib.mkShell {
         inherit inputs pkgs;
         modules = [
           ({ pkgs, config, ... }: {
@@ -72,5 +69,6 @@
           })
         ];
       };
-    };
+    });
+
 }
