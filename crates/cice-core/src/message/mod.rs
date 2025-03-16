06a2f85bbb0b4e55a95b9b1a7590d4f8aafbb365
chiichen <chiichen@qq.com>
2025-03-16 20:45:53 +0800

    feat(core): mark Message non_exhaustive

--- crates/cice-core/src/message/mod.rs
+++ crates/cice-core/src/message/mod.rs
@@ -4,6 +4,7 @@ use task::TaskMessage;
 pub mod task;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[non_exhaustive] //It's likely to be extended at any time. So keep this for compatibility
 pub enum Message {
     TaskMessage(TaskMessage),
 }
