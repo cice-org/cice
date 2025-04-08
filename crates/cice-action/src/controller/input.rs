8c5b1f752a11070410b8d077c9672ac55c5f2837
chiichen <chiichen@qq.com>
2025-04-08 20:52:05 +0800

    feat(controller@adb): basic support adb controller

--- crates/cice-action/src/controller/input.rs
+++ crates/cice-action/src/controller/input.rs
@@ -1,22 +1,36 @@
-use euclid::{Box2D, Point2D};
 use serde::{Deserialize, Serialize};
 
-use crate::Action;
-
-pub type Px = ();
-pub type Point = Point2D<u64, Px>;
-pub type Rect = Box2D<u64, Px>;
+use crate::types::{Point, Rect};
 
 #[derive(Serialize, Deserialize)]
-#[serde(untagged)]
+#[non_exhaustive]
 pub enum ControllerInputAction {
-    Click(ClickPostion),
+    #[serde(rename = "click")]
+    Click { pos: ClickPostion },
+    #[serde(rename = "swipe")]
+    Swipe { from: Point, to: Point },
+    #[serde(rename = "keyevent")]
+    KeyEvent { id: usize },
 }
-
-impl Action for ControllerInputAction {}
 
 #[derive(Serialize, Deserialize)]
 pub enum ClickPostion {
     Point(Point),
     Rect(Rect),
+}
+
+#[cfg(test)]
+mod test {
+    use crate::controller::input::{ControllerInputAction, Point};
+
+    #[test]
+    fn deserialze() {
+        println!(
+            "{:?}",
+            serde_json::to_string(&ControllerInputAction::Swipe {
+                from: Point::zero(),
+                to: Point::new(200, 200)
+            })
+        )
+    }
 }
