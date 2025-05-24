d125eba204acf9158cf3b5f13306320bf075dc4c
chiichen <chiichen@qq.com>
2025-05-24 23:10:56 +0800

    test(core): add test for image output

--- crates/cice-core/tests/common/controller/mod.rs
+++ crates/cice-core/tests/common/controller/mod.rs
@@ -1,5 +1,7 @@
+mod controller_simple_image_output;
 mod controller_simple_with_config;
 
+pub use controller_simple_image_output::*;
 pub use controller_simple_with_config::*;
 
 #[derive(serde::Deserialize, serde::Serialize, Clone)]
