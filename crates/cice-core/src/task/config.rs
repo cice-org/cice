37d23f4f912bdacf5cde5937a077521998ee8d3e
chiichen <chiichen@qq.com>
2025-05-27 22:54:35 +0800

    refactor: Refactor task config into mod.rs and update type definitions

--- crates/cice-core/src/task/config.rs
+++
@@ -1,8 +0,0 @@
-#[derive(serde::Serialize, serde::Deserialize, Clone, PartialEq)]
-pub struct BaseTaskConfig {
-    pub task_name: String,
-    pub next_task: Vec<String>,
-    pub interrupt_task: Vec<String>,
-    pub controller_id: String,
-    pub recognizer_id: String,
-}
